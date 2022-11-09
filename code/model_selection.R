24ab25a08dfa7a2aa2c7598851aed7b8f2904bdd
jswesner <jeff.wesner@usd.edu>
2022-11-09 14:47:32 -0600

    fixed mistake with natural logs

--- code/model_selection.R
+++ code/model_selection.R
@@ -2,9 +2,16 @@ library(tidyverse)
 library(brms)
 library(tidybayes)
 
-d_temp = read_csv("ccsr_colonybased_summaries_2022_10_27.csv")
 
-d_temp_c = d_temp %>% select(uid, site_id, temp_mean) %>% 
+# get the fitted models
+mod_a = readRDS(file = "models/mod_a.rds")
+mod_b = readRDS(file = "models/mod_b.rds")
+mod_c = readRDS(file = "models/mod_c.rds")
+mod_d = readRDS(file = "models/mod_d.rds")
+
+# load data
+d_temp = read_csv("ccsr_colonybased_summaries_2022_10_27.csv") %>% 
+  select(uid, site_id, temp_mean) %>% 
   mutate(temp_c = scale(temp_mean))
 
 d_cell_based = read_csv("ccsr_cellbased_summaries_2020_11_09.csv") %>% 
@@ -12,20 +19,20 @@ d_cell_based = read_csv("ccsr_cellbased_summaries_2020_11_09.csv") %>%
   left_join(d_temp_c) %>% 
   mutate(zoo_total_biomass = zoo_mean_biomass*zoo_total_density)
 
-d_cell_based_centered = d_cell_based %>% 
-  mutate(log_nit = log(nitrate_mean),
-         log_phos = log(phos_total_mean),
-         log_zoo = log(zoo_total_biomass),
-         log_biovol = log(phyto_mean_biovol),
-         log_phyto = log(phyto_total_density)) %>% 
-  mutate(nit_c = scale(log_nit),
-         pho_c = scale(log_phos),
-         zoo_c = scale(log_zoo),
-         biovol_c = scale(log_biovol))
+d_cell_based_logcentered = d_cell_based %>% 
+  mutate(log_nit = log10(nitrate_mean),
+         log_phos = log10(phos_total_mean),
+         log_zoo = log10(zoo_total_biomass),
+         log_biovol = log10(phyto_mean_biovol),
+         log_phyto = log10(phyto_total_density)) %>% 
+  mutate(nit_c = scale(log_nit, scale = F),
+         pho_c = scale(log_phos, scale = F),
+         zoo_c = scale(log_zoo, scale = F),
+         biovol_c = scale(log_biovol, scale = F))
 
 mod_a = brm(log_phyto ~ biovol_c*temp_c,
             family = gaussian(),
-            data = d_cell_based_centered,
+            data = d_cell_based_logcentered,
             prior = c(prior(normal(0, 1), class = "b"),
                       prior(normal(-0.875, 0.2), coef = "biovol_c"),
                       prior(normal(10, 2), class = "Intercept"),
---
+++ code/plotting.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(brms)
+library(tidybayes)
+
+
+# get the fitted models
+mod_a = readRDS(file = "models/mod_a.rds")
+mod_b = readRDS(file = "models/mod_b.rds")
+mod_c = readRDS(file = "models/mod_c.rds")
+mod_d = readRDS(file = "models/mod_d.rds")
+
+
+mod_c
+
